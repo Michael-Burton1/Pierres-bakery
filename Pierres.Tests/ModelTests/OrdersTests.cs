4ae3bde7b00e8dcb3863c8a3f938d94eb3c2e987
Michael Burton <micbur1@gmail.com>
2021-05-21 14:47:40 -0700

    add a test to be able to 'set' an order's details

--- Pierres.Tests/ModelTests/OrdersTests.cs
+++ Pierres.Tests/ModelTests/OrdersTests.cs
@@ -3,27 +3,45 @@ using Sysytem.Collections.Generic;
 using Sysytem;
 using Pierres.Models;
 
-namespace Pierres.TestTOols
+namespace Pierres.TestTools
 {
   [TestClass]
   public class OrderTests : IDisposable
+  {
 
-public void Dispose()
-  {
-    Order.ClearAll();
-  }
-  [TestMethod]
-  public void OrderConstructor_CreatesAnOrderObject_Order()
-  {
-    Order myOrder = new Order("test");
-    Assert.AreEqual(typeof(Order), myOrder.GetType());
-  }
-  [TestMethod]
-  public void GetDetails_retunsOrderDetails_string()
-  {
-    string details = "12 hot crossed buns";
-    Order myOrder = new Order(details);
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
 
-    Assert.AreEqual(details, result);
+    [TestMethod]
+    public void OrderConstructor_CreatesAnOrderObject_Order()
+    {
+      Order myOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), myOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetDetails_retunsOrderDetails_string()
+    {
+      string details = "12 hot crossed buns";
+      Order myOrder = new Order(details);
+
+      Assert.AreEqual(details, result);
+    }
+
+    [TestMethod]
+    public void SetDetails_SetDetails_String()
+    {
+      string details = "12 Hot cross buns.";
+      Order myOrder = new Order(details);
+
+      string updatedDetails = "24 million hot crossed buns";
+      myOrder.Details = updatedDetails;
+      string result = myOrder.Details;
+
+      Assert.AreEqual(updatedDetails, result);
+    }
+
   }
 }

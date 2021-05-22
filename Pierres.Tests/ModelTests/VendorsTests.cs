440f44a3bcf9ca40740be73ae703628a52d487ac
Michael Burton <micbur1@gmail.com>
2021-05-22 15:31:47 -0700

    add new test for vendors class to be able to assosiate order with vendor

--- Pierres.Tests/ModelTests/VendorsTests.cs
+++ Pierres.Tests/ModelTests/VendorsTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 using Pierres.Models;
 using System.Collections.Generic;
+using System;
 
 namespace Pierres.Tests
 {
@@ -23,7 +23,7 @@ namespace Pierres.Tests
     [TestMethod]
     public void GetName_ReturnsName_String()
     {
-      string name = new Vendor(name);
+      string name = "Game Of Scones";
       Vendor newVendor = new Vendor(name);
 
       string result = newVendor.Name;
@@ -35,24 +35,61 @@ namespace Pierres.Tests
     public void GetId_RetunsVendorId_Int()
     {
       string name = "Count Chocula's Discount Chocolate Biscuits";
-      Vendor NewVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name);
 
-      int result = NewVendor.Id;
+      int result = newVendor.Id;
 
       Assert.AreEqual(1, result);
     }
 
+
+
     [TestMethod]
     public void GetAll_RetunsAllVendorsObjectsa_VendorsList()
     {
-      string name1 = "Freddy Kruger's Killer Bread";
-      string name2 = "Cardi B's WAP: Waffles and Pancakes";
+      string name01 = "Let's get this Bread";
+      string name02 = "Joleen, I'm begging of you plz don't take my ham.";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      //Act
+      List<Vendor> result = Vendor.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
+
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name1 = "Freddy Krugger's Killer Bread";
+      string name2 = "Cardi B's Waffles and Pancakes";
       Vendor newVendor1 = new Vendor(name1);
       Vendor newVendor2 = new Vendor(name2);
 
-      Vendor reuslt = Vendor.Find(2);
+      Vendor result = Vendor.Find(2);
 
       Assert.AreEqual(newVendor2, result);
+    }
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      //Arrange
+      string details = "the world's largest muffin.";
+      Order myOrder = new Order(details);
+      List<Order> newList = new List<Order> { myOrder };
+      string name = "Cathy's SN-'Ack'-bar";
+      Vendor newVendor = new Vendor(name);
+      newVendor.AddOrder(myOrder);
+
+      //Act
+      List<Order> result = newVendor.Orders;
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 

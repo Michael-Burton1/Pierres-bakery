b49f246f420b03c24dacff4bf9fa047e7d415a41
Michael Burton <micbur1@gmail.com>
2021-05-22 20:03:12 -0700

    renamed file to be able to access the correct paths in project

---
+++ Pierres/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Pierres.Models
+{
+  public class Order
+  {
+    public string Details { get; set; }
+    public int ID { get; }
+    private static List<Order> _instances = new List<Order> { };
+
+    public Order(string details)
+    {
+      Details = details;
+      _instances.Add(this);
+      ID = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+}

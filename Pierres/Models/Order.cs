41cd44485c178063c247f2f6e5f1ae30aaba2fba
Michael Burton <micbur1@gmail.com>
2021-05-23 16:48:54 -0700

    add date and price properties to the order class to be able to pass the modifyed test that include testing for those properties

--- Pierres/Models/Order.cs
+++ Pierres/Models/Order.cs
@@ -5,12 +5,16 @@ namespace Pierres.Models
   public class Order
   {
     public string Details { get; set; }
+    public string Date { get; set; }
+    public double Price { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
-    public Order(string details)
+    public Order(string details, string date, double price)
     {
       Details = details;
+      Date = date;
+      Price = price;
       _instances.Add(this);
       Id = _instances.Count;
     }

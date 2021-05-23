da013323267d413106ce6c2745ea0b8819d631e3
Michael Burton <micbur1@gmail.com>
2021-05-23 16:51:03 -0700

    delete commented out code that was not being used (no longer used routes)

--- Pierres/Controllers/OrdersController.cs
+++ Pierres/Controllers/OrdersController.cs
@@ -13,12 +13,6 @@ namespace Pierres.Controllers
       return View(vendor);
     }
 
-    // [HttpPost("/orders/delete")]
-    // public ActionResult DeleteAll()
-    // {
-    //   Order.ClearAll();
-    //   return View();
-    // }
 
     [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
     public ActionResult Show(int vendorId, int orderId)
@@ -31,38 +25,5 @@ namespace Pierres.Controllers
       return View(model);
     }
 
-    //   [HttpGet("/orders")]
-    //   public ActionResult Index()
-    //   {
-    //     List<Order> allOrders = Order.GetAll();
-    //     return View(allOrders);
-    //   }
-
-    //   [HttpGet("/orders/new")]
-    //   public ActionResult New()
-    //   {
-    //     return View();
-    //   }
-
-    //   [HttpPost("/orders")]
-    //   public ActionResult Create(string description)
-    //   {
-    //     Order myOrder = new Order(description);
-    //     return RedirectToAction("Index");
-    //   }
-
-    //   [HttpPost("/orders/delete")]
-    //   public ActionResult DeleteAll()
-    //   {
-    //     Order.ClearAll();
-    //     return View();
-    //   }
-
-    //   [HttpGet("/orders/{id}")]
-    //   public ActionResult Show(int id)
-    //   {
-    //     Order foundOrder = Order.Find(id);
-    //     return View(foundOrder);
-    //   }
   }
 }

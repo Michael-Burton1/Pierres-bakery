74be4f85ae8ca135c9638611e7eb037007b10701
Michael Burton <micbur1@gmail.com>
2021-05-23 16:46:54 -0700

    modify test to include pdate and price properties for orders

--- Pierres.Tests/ModelTests/OrdersTests.cs
+++ Pierres.Tests/ModelTests/OrdersTests.cs
@@ -17,7 +17,7 @@ namespace Pierres.TestTools
     [TestMethod]
     public void OrderConstructor_CreatesAnOrderObject_Order()
     {
-      Order myOrder = new Order("test");
+      Order myOrder = new Order("test", "now", 1);
       Assert.AreEqual(typeof(Order), myOrder.GetType());
     }
 
@@ -25,18 +25,26 @@ namespace Pierres.TestTools
     public void GetDetails_retunsOrderDetails_string()
     {
       string details = "12 hot crossed buns";
+      string date = "tuesday";
+      double price = 10.50;
 
-      Order myOrder = new Order(details);
-      string result = myOrder.Details;
+      Order myOrder = new Order(details, date, price);
+      string detailsResult = myOrder.Details;
+      string dateResult = myOrder.Date;
+      double priceResult = myOrder.Price;
 
-      Assert.AreEqual(details, result);
+      Assert.AreEqual(details, detailsResult);
+      Assert.AreEqual(date, dateResult);
+      Assert.AreEqual(price, priceResult);
     }
 
     [TestMethod]
     public void SetDetails_SetDetails_String()
     {
       string details = "12 Hot cross buns.";
-      Order myOrder = new Order(details);
+      string date = "tuesday";
+      double price = 10.50;
+      Order myOrder = new Order(details, date, price);
 
       string updatedDetails = "24 million hot crossed buns";
       myOrder.Details = updatedDetails;
@@ -59,8 +67,8 @@ namespace Pierres.TestTools
     {
       String myDetails = "12 hot cross buns";
       String yourDetails = "4 and 20 blackbirds baked into a pie.";
-      Order newOrder1 = new Order(myDetails);
-      Order newOrder2 = new Order(yourDetails);
+      Order newOrder1 = new Order(myDetails, "wow", 4);
+      Order newOrder2 = new Order(yourDetails, "oh no", 8);
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
 
       List<Order> result = Order.GetAll();
